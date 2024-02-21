2df7c3de5bd0772e1aa7ed3fdc27b3c652c7b077
wuva <133061732+rafinhafreire@users.noreply.github.com>
2024-02-21 17:58:54 -0400

    Add files via upload

---
+++ aulinha de java/src/aulinha/de/java/Jlabel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulinha.de.java;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Jlabel extends JLabel {
+
+    public Jlabel(String label_with_text) {
+    }
+    
+}
---
+++ aulinha de java/src/newpackage/LabelTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+/**
+ *
+ * @author Aluno
+ */
+
+import javax.swing.JFrame;
+
+public class LabelTest {
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize( 260, 180);
+        labelFrame.setVisible(true);
+    }
+    
+    
+}
